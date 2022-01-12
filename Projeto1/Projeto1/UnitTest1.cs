65b0926670edfceb06245691a5fe3ab56785e115
GustavoBoscoGit <gustavobosco1@gmail.com>
2022-01-12 17:04:23 -0300

    first class

--- Projeto1/Projeto1/UnitTest1.cs
+++ Projeto1/Projeto1/UnitTest1.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 using System;
 
 namespace Projeto1
@@ -9,7 +11,23 @@ namespace Projeto1
         [TestMethod]
         public void TestMethod1()
         {
-            Console.WriteLine("teste selenium");
+            //create a reference to chrome browser
+            IWebDriver driver = new ChromeDriver();
+
+            // visit a web page
+            driver.Navigate().GoToUrl("https://google.com");
+
+            //full screen
+            driver.Manage().Window.Maximize();
+
+            //find the element 
+            IWebElement searchBox = driver.FindElement(By.Name("q"));
+
+            //type something in the search bar
+            searchBox.SendKeys("hello world");
+
+            //close the browser
+            //driver.Quit(); 
         } 
     }
 
